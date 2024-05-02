2e1299b261d8c35bc4a500c6e5f279755fb6d1e4
dede <deakurniah@gmail.com>
2024-05-02 11:37:38 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import PokemonList from "./components/PokemonList/PokemonList"
+
+function App() {
+  return (
+    <>
+      <PokemonList />
+    </>
+  );
+}
+
+export default App
